87844fda461a44849e3ee5f4cb1cb393e8a1f605
P Dalwadi <dalwad63@rowan.edu>
2026-01-12 15:43:04 -0500

    Adding a file

--- src/basic_calc/calcMain.java
+++ src/basic_calc/calcMain.java
@@ -6,8 +6,47 @@ public class calcMain {
 
 	public static void main(String[] args) {
 		
+		Scanner s = new Scanner(System.in);
 		
-
+		while(true) {
+			System.out.print("Enter the operation (+,-,*,/,%, or exit): ");
+			String op = s.next();
+			
+			// ==: compares memory address while .equals() compares actual content
+			if(op.equals("exit")) {
+				break; // only breaks the loop, still runs everything outside of loop
+			}
+			
+			System.out.print("Enter the first number: ");
+			int num1 = s.nextInt();
+			
+			System.out.print("Enter the second number: ");
+			int num2 = s.nextInt();
+			
+			switch(op) {
+				case "+": int add = num1 + num2;
+				System.out.println("Result: " + add);
+				break;
+				case "-": int sub = num1 - num2;
+				System.out.println("Result: " + sub);
+				break;
+				case "*": int mult = num1 * num2;
+				System.out.println("Result: " + mult);
+				break;
+				case "/": int div = num1 / num2;
+				System.out.println("Result: " + div);
+				break;
+				case "%": int rem = num1 % num2;
+				System.out.println("Result: " + rem);
+				break;
+				default:
+					System.out.println("Invalid operation" + op);
+					continue;
+				}
+			
+		}
+		System.out.println("Thanks for using this calculator!");
+		s.close();
+	
 	}
-
 }
